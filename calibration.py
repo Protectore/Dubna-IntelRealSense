dddecb08933baed8c624c4d566d3d8585843838d
Denis Gurkov <markelovonelove@yandex.ru>
2021-06-07 15:54:13 +0300

    Файл калибровки отредактирован

--- calibration.py
+++ calibration.py
@@ -1,19 +1,29 @@
+"""Данный модуль необходим для автоматической калибровки камеры Intel Real Sense"""
 import pyrealsense2 as rs2
-import numpy as np
-import cv2
-
-# Once started successfully, dev object can be casted to rs2.auto_calibrated_device by calling:
-pipe = rs2.pipeline()
-cfg = rs2.config()
-cfg.enable_stream(rs2.stream.depth, 256, 144, rs2.format.z16, 90)
-dev = pipe.start(cfg).get_device()
-
-# Once started successfully, dev object can be casted to rs2.auto_calibrated_device by calling:
-cal = rs2.auto_calibrated_device(dev)
 
 
-def cb(progress):
-    print(".")
+def calibrate(image_width=256, image_height=144, frame_rate=90, calibration_settings='', timeout=15000):
+    # Once started successfully, dev object can be casted to rs2.auto_calibrated_device by calling:
+    pipeline = rs2.pipeline()
+    config = rs2.config()
+
+    config.enable_stream(rs2.stream.depth,
+                         image_width, 
+                         image_height,
+                         rs2.format.z16,
+                         frame_rate)
+    device = pipeline.start(config).get_device()
+
+    # Once started successfully, dev object can be casted to rs2.auto_calibrated_device by calling:
+    calibrated_device = rs2.auto_calibrated_device(device)
+
+    result, health = calibrated_device.run_on_chip_calibration(calibration_settings,
+                                                                lambda _ : print('.'),
+                                                                timeout)
+
+    return result, health
 
 
-res, health = cal.run_on_chip_calibration("", cb, 15000)
+if __name__ == '__main__':
+    result, health = calibrate()
+    print(health)
