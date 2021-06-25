a3cc45e092722bbb5cd4049243545cebe70cfa4a
Protectore <kostyagazalov@mail.ru>
2021-06-25 14:31:09 +0300

    Доработан скрипт для измерения размера объектов по фото, первая рабочая версия

--- measurement_test.py
+++ measurement_test.py
@@ -3,36 +3,72 @@ import cv2
 import numpy as np
 
 
+DEPTH_MIN, DEPTH_MAX = 0.4, 10  # В метрах
+WIDTH, HEIGHT = 1280, 720
+FPS = 30
+
 SELECTION_COLOR = (0, 255, 0)
 LINE_THICKNESS = 1
 start, stop = None, None
+depth_start, depth_stop = None, None
 size = None
 
 
 def mouse_events_handler(event, x, y, flags, param):
-    global depth_frame, depth_colormap, depth_intrinsics, depth_colormap_to_show, color_frame, color_frame_to_show, start, stop, size
+    # Объявляем глобальные переменные. Первой строчкой идут изображения, отображаемые
+    # в окнах, во второй идут переменные для отрисовки и вычисления размера линии
+    global depth_colormap_to_show, color_frame_to_show
+    global start, stop, size
+    global depth_start, depth_stop
     if event == cv2.EVENT_LBUTTONDOWN:
         start = (x, y)
         stop = start
-        size = rs.rs2_deproject_pixel_to_point(depth_intrinsics, [y, x], depth_frame[y, x])
+        # Вычисляем координаты выбранной точки цветной картинки на карте глубины
+        depth_start = rs.rs2_project_color_pixel_to_depth_pixel(
+            frame.get_depth_frame().get_data(), depth_scale,
+            DEPTH_MIN, DEPTH_MAX,
+            depth_intrinsics, color_intrinsics, depth_to_color_extrinsics,
+            color_to_depth_extrinsics, start)
+        # Преобразуем к инту, opencv иначе ругается
+        depth_start = [int(i) for i in depth_start]
+        depth_stop = depth_start
+        # На самом деле пока что это не размер, а координаты проекции начальной точки в пространстве
+        size = rs.rs2_deproject_pixel_to_point(depth_intrinsics, depth_start[::-1], depth_frame[depth_start[1], depth_start[0]])
         print(f'start: x = {x}, y = {y}, coords = {size}')
-        cv2.line(depth_colormap_to_show, start, stop, SELECTION_COLOR, LINE_THICKNESS)
-        cv2.line(color_frame_to_show, start, stop, SELECTION_COLOR, LINE_THICKNESS)
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if (start):
             depth_colormap_to_show = np.copy(depth_colormap)
             color_frame_to_show = np.copy(color_frame)
             stop = (x, y)
-            cv2.line(depth_colormap_to_show, start, stop, SELECTION_COLOR, LINE_THICKNESS)
-            cv2.line(color_frame_to_show, start, stop, SELECTION_COLOR, LINE_THICKNESS)
+            depth_stop = rs.rs2_project_color_pixel_to_depth_pixel(
+                frame.get_depth_frame().get_data(), depth_scale,
+                DEPTH_MIN, DEPTH_MAX,
+                depth_intrinsics, color_intrinsics, depth_to_color_extrinsics,
+                color_to_depth_extrinsics, stop)
+            depth_stop = [int(i) for i in depth_stop]
+            # Тут иногда может возникать ошибка, наверное, нужно исправить?
+            try:
+                cv2.line(depth_colormap_to_show, depth_start, depth_stop, SELECTION_COLOR, LINE_THICKNESS)
+                cv2.line(color_frame_to_show, start, stop, SELECTION_COLOR, LINE_THICKNESS)
+            except Exception as exc:
+                print('Ашипка')
+                print(depth_stop)
+                print(exc)
 
     elif event == cv2.EVENT_LBUTTONUP:
         stop = (x, y)
-        cv2.line(depth_colormap_to_show, start, stop, SELECTION_COLOR, LINE_THICKNESS)
+        depth_stop = rs.rs2_project_color_pixel_to_depth_pixel(
+            frame.get_depth_frame().get_data(), depth_scale,
+            DEPTH_MIN, DEPTH_MAX,
+            depth_intrinsics, color_intrinsics, depth_to_color_extrinsics,
+            color_to_depth_extrinsics, stop)
+        depth_stop = [int(i) for i in depth_stop]
+        cv2.line(depth_colormap_to_show, depth_start, depth_stop, SELECTION_COLOR, LINE_THICKNESS)
         cv2.line(color_frame_to_show, start, stop, SELECTION_COLOR, LINE_THICKNESS)
-        end_coords = rs.rs2_deproject_pixel_to_point(depth_intrinsics, [y, x], depth_frame[y, x])
+        end_coords = rs.rs2_deproject_pixel_to_point(depth_intrinsics, depth_stop[::-1], depth_frame[depth_stop[1], depth_stop[0]])
         print(f'stop: x = {x}, y = {y}, coords = {end_coords}')
+        # Если начальная/конечная точки попали туда, где не вычисленна глубина, вычисления невозможно выполнить
         if (np.argmax(size) == 0 or np.argmax(end_coords) == 0):
             print('Eror, start or end depth isn\'t defined')
         else:
@@ -42,94 +78,91 @@ def mouse_events_handler(event, x, y, flags, param):
             print(f'height = {round(size[0], 2)}mm')
             print(f'depth = {round(size[2], 2)}mm')
             print(f'vector norm = {round(np.linalg.norm(size), 2)}mm')
-        start = None
-        stop = None
+        start, stop = None, None
+        depth_start, depth_stop = None, None
         size = None
         print()
 
 
-def convert_depth_to_phys_coord_using_realsense(x, y, depth, cameraInfo):
-    _intrinsics = rs.intrinsics()
-    _intrinsics.width = cameraInfo.width
-    _intrinsics.height = cameraInfo.height
-    _intrinsics.ppx = cameraInfo.K[2]
-    _intrinsics.ppy = cameraInfo.K[5]
-    _intrinsics.fx = cameraInfo.K[0]
-    _intrinsics.fy = cameraInfo.K[4]
-    #_intrinsics.model = cameraInfo.distortion_model
-    _intrinsics.model = rs.distortion.none
-    _intrinsics.coeffs = [i for i in cameraInfo.D]
-    result = rs.rs2_deproject_pixel_to_point(_intrinsics, [y, x], depth)
-    # result[0]: right, result[1]: down, result[2]: forward
-    return result[2], -result[0], -result[1]
-
-
-def get_image(frame: rs.composite_frame) -> (np.array, np.array):
-    """
-    Возвращает цветное изображение и depth frame
-    Arguments:
-    frame - Объект composite_frame, содержащий данные для считывания.
-    """
-    color_frame = np.asanyarray(frame.get_color_frame().get_data())
-    depth_frame = frame.get_depth_frame()
-    depth_frame = np.asanyarray(depth_frame.get_data()).astype(float)
-    return color_frame, depth_frame
-
-
-def take_photo(pipeline: rs.pipeline) -> (([bool, rs.composite_frame]), np.array, np.array, np.array):
+def take_photo(pipeline: rs.pipeline) -> (np.array, np.array, np.array):
     """
     !!!!!!!!!!!!!!!!!!!!!!
     Возвращает всякие штуки, напишите, пожалуйста, по человечески
     !!!!!!!!!!!!!!!!!!!!!!
     pipeline - Объект pipeline, обрабатвающий поток с камеры.
     """
-    frame = pipeline.wait_for_frames()
-    color_frame, depth_frame = get_image(frame)
-    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_frame, alpha=0.03), cv2.COLORMAP_JET)
-    color_frame = cv2.cvtColor(color_frame, cv2.COLOR_BGR2RGB)
-    color_frame = cv2.resize(color_frame, (depth_colormap.shape[1], depth_colormap.shape[0]))
+    frame = pipeline.wait_for_frames()  # Получаем кадр
+
+    color_frame = np.asanyarray(frame.get_color_frame().get_data())  # Достаём цветную картинку
+
+    depth_frame = np.asanyarray(frame.get_depth_frame().get_data())  # Достаём глубину. Тут ещё .astype(float) был
+    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_frame, alpha=0.03), cv2.COLORMAP_JET)  # Делаем картинку на основе глубины
+    # color_frame = cv2.resize(color_frame, (depth_colormap.shape[1], depth_colormap.shape[0]))
     return frame, color_frame, depth_frame, depth_colormap
 
 
 pipeline = rs.pipeline()
 config = rs.config()
 
+# Настройка каналов потока
+config.enable_stream(rs.stream.depth, WIDTH, HEIGHT, rs.format.z16, FPS)
+config.enable_stream(rs.stream.color, WIDTH, HEIGHT, rs.format.bgr8, FPS)
+
 pipeline_wrapper = rs.pipeline_wrapper(pipeline)
 pipeline_profile = config.resolve(pipeline_wrapper)
 device = pipeline_profile.get_device()
 
-#config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-#config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 # Start streaming
 pipeline.start(config)
-for _ in range(10):
+# Религиозная традиция, пропускать первые 5 кадров
+for _ in range(5):
     pipeline.wait_for_frames()
 
-# Get stream profile and camera intrinsics
+# Полуаем профиль потока
 profile = pipeline.get_active_profile()
-depth_profile = rs.video_stream_profile(profile.get_stream(rs.stream.depth))
-depth_intrinsics = depth_profile.get_intrinsics()
 
+# Тут достаём всякие штуки для маппинга цветных координат с координатами глубины.
+# !!!!!
+depth_scale = device.first_depth_sensor().get_depth_scale()
+depth_stream = profile.get_stream(rs.stream.depth)
+color_stream = profile.get_stream(rs.stream.color)
+depth_intrinsics = depth_stream.as_video_stream_profile().get_intrinsics()  # Эта штука нужна для первоначального кода
+color_intrinsics = color_stream.as_video_stream_profile().get_intrinsics()
+depth_to_color_extrinsics = depth_stream.as_video_stream_profile().get_extrinsics_to(color_stream)
+color_to_depth_extrinsics = color_stream.as_video_stream_profile().get_extrinsics_to(depth_stream)
+# !!!!!
+
+# Собираем необходимые данные при запуске
 frame, color_frame, depth_frame, depth_colormap = take_photo(pipeline)
+# Делаем копии изображений для отображения. Они нужны чтобы нарисованные линии
+# не записывались на оригиналы
 color_frame_to_show = np.copy(color_frame)
 depth_colormap_to_show = np.copy(depth_colormap)
 
 if depth_frame.shape != color_frame.shape:
     depth_frame.resize((color_frame.shape[0], color_frame.shape[1]))
 
+# Создаём окна для отображения карты глубины и цветной картинки
 cv2.namedWindow('depth colormap')
 cv2.namedWindow('image')
-cv2.setMouseCallback('depth colormap', mouse_events_handler)
+# Вешаем обработчик событий мыши
+# cv2.setMouseCallback('depth colormap', mouse_events_handler)
 cv2.setMouseCallback('image', mouse_events_handler)
+# Основной цикл
 while(1):
+    # Отображаем изображения
     cv2.imshow('depth colormap', depth_colormap_to_show)
     cv2.imshow('image', color_frame_to_show)
     key = cv2.waitKey(1)
+    # При нажатии клавиши p (английская p, раскладка учитывается)
     if key == ord('p'):
         print('taking photo...')
+        # Получаем новый кадр и делаем его копии
         frame, color_frame, depth_frame, depth_colormap = take_photo(pipeline)
         color_frame_to_show = np.copy(color_frame)
         depth_colormap_to_show = np.copy(depth_colormap)
         print('done\n')
+    # При нажатии Esc
     elif key & 0xFF == 27:
+        # Выход из программы
         break
